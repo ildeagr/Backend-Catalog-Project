9ff56f22db8d27cada3691142a2f332f340abe63
Ildefonso Albares Garcia <ilde_07@icloud.com>
2024-10-16 13:13:22 +0200

    Delete sprintboot-servicio-catalogo directory

--- sprintboot-servicio-catalogo/src/main/resources/impor.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO marcas (nombre, precio, articulo) VALUES('Panasonic', 800, 'Ordenador');
-INSERT INTO marcas (nombre, precio, articulo) VALUES('Nike', 700, 'Zapatillas');
-INSERT INTO marcas (nombre, precio, articulo) VALUES('Apple', 1000, 'Movil');
-INSERT INTO marcas (nombre, precio, articulo) VALUES('Sony Notebook', 1000, 'Ordenador');
-INSERT INTO marcas (nombre, precio, articulo) VALUES('Hewlett Packard', 500, 'Ordenador');
-INSERT INTO marcas (nombre, precio, articulo) VALUES('Adidas2', 600, 'Zapatillas');
-INSERT INTO marcas (nombre, precio, articulo) VALUES('Nike2', 100, 'Zapatillas');
-INSERT INTO marcas (nombre, precio, articulo) VALUES('Adidas3', 200, 'Zapatillas');
-INSERT INTO marcas (nombre, precio, articulo) VALUES('Reebok', 300, 'Zapatillas');
